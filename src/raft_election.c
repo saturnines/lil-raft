93a48ef8184fea4bd83e1247d37504b55d5cf707
Kevin <kevisos123@gmail.com>
2025-12-14 16:18:15 -0600

    Added dummy raft election and replication files for testing core.

---
+++ src/raft_election.c
@@ -0,0 +1,13 @@
+/**
+* raft_election.c - RequestVote RPC logic
+ *
+ * TODO: Implement properly
+ */
+
+#include "raft_internal.h"
+
+// Stub for now
+void raft_send_requestvote_all(raft_t *r) {
+    (void)r;
+    // TODO: Send RequestVote to all peers
+}
---
+++ src/raft_replication.c
@@ -0,0 +1,13 @@
+/**
+* raft_replication.c - AppendEntries RPC logic
+ *
+ * TODO: Implement properly
+ */
+
+#include "raft_internal.h"
+
+// Stub for now
+void raft_send_heartbeats(raft_t *r) {
+    (void)r;
+    // TODO: Send heartbeats to all peers
+}
